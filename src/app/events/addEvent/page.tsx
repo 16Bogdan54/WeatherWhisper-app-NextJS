8d82554e6f5a111be354e96dc5301267d90e54d6
16Bogdan54 <b.yunakov1654@gmail.com>
2023-06-04 23:16:18 +0300

    hadnling

--- src/app/events/addEvent/page.tsx
+++ src/app/events/addEvent/page.tsx
@@ -1,27 +1,60 @@
 "use client";
 
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Button, Label, Textarea, TextInput } from "flowbite-react";
 
-export default async function addEventPage() {
+interface EventFormInput {
+  title: String;
+  author: String;
+  description: String;
+}
+
+export default async function AddEventPage() {
+  const { register, handleSubmit } = useForm<EventFormInput>();
+  const onSubmit: SubmitHandler<EventFormInput> = (data) =>
+    console.log("event", data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <div className="mb-2 block">
-          <Label className="text-white" htmlFor="base" value="Title" />
+          <Label className="text-white" htmlFor="title" value="Title" />
         </div>
-        <TextInput id="base" sizing="md" type="text" placeholder="Title" />
+        <TextInput
+          {...register("title")}
+          id="title"
+          sizing="md"
+          type="text"
+          placeholder="Title"
+        />
       </div>
       <div>
         <div className="mb-2 block">
-          <Label className="text-white" htmlFor="base" value="Author" />
+          <Label className="text-white" htmlFor="author" value="Author" />
         </div>
-        <TextInput id="base" sizing="md" type="text" placeholder="Author" />
+        <TextInput
+          {...register("author")}
+          id="author"
+          sizing="md"
+          type="text"
+          placeholder="Author"
+        />
       </div>
       <div className="max-w-md mb-4" id="textarea">
         <div className="mb-2 block">
-          <Label className="text-white" htmlFor="comment" value="Description" />
+          <Label
+            className="text-white"
+            htmlFor="description"
+            value="Description"
+          />
         </div>
-        <Textarea id="comment" placeholder="Description" required rows={3} />
+        <Textarea
+          {...register("description")}
+          id="description"
+          placeholder="Description"
+          required
+          rows={3}
+        />
       </div>
       <Button type="submit">Submit</Button>
     </form>
