6c9dc5d0d1dda7bc66e570bb2134626bc8c56814
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-07-25 19:05:02 +0300

    project setup

--- src/lib/getUser.ts
+++
@@ -1,7 +0,0 @@
-export const getUser = async (userId: string) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-
-    if(!res.ok) return undefined;
-
-    return res.json();
-}
