a0064f4f535d461f2a6983f2e325e76f20b1b099
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-07-26 18:36:17 +0300

    configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      text_color: "#040511",
+      background_color: "#dee0f7",
+      primary_color: "#2b39b6",
+      secondary_color: "#bdc2ef",
+      accent_color: "#2b39b6",
     },
   },
   plugins: [],
-}
+};
---
+++ types.d.ts
@@ -0,0 +1,45 @@
+type WeatherData = {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  current_weather: {
+    temperature: number;
+    windspeed: number;
+    winddirection: number;
+    weathercode: number;
+    is_day: number;
+    time: number;
+  };
+  hourly_units: {
+    time: string;
+    temperature_2m: string;
+    relativehumidity_2m: string;
+  };
+  hourly: {
+    time: number[];
+    temperature_2m: number[];
+    relativehumidity_2m: number[];
+  };
+  daily_units: {
+    time: string;
+    weathercode: string;
+    temperature_2m_max: string;
+    temperature_2m_min: string;
+    apparent_temperature_max: string;
+    apparent_temperature_min: string;
+    precipitation_sum: string;
+  };
+  daily: {
+    time: number[];
+    weathercode: number[];
+    temperature_2m_max: number[];
+    temperature_2m_min: number[];
+    apparent_temperature_max: number[];
+    apparent_temperature_min: number[];
+    precipitation_sum: number[];
+  };
+};
