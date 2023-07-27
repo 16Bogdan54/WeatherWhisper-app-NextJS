e13b6cf7133d9a1cc1c59f13b23dcf007bc6a725
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-07-27 21:08:06 +0300

    logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
+import Link from "next/link";
 import style from "./Logo.module.css";
 
 const Logo = () => {
   const [isHover, setHover] = useState<boolean>(false);
 
   const getTextClass = (isHover: boolean) => {
-    return isHover ? "WeatherWhisper" : "WW";
+    return isHover ? "" : style.text_hidden;
   };
 
   return (
-    <>
+    <Link href="/">
       <h1
         className={style.logo}
         onMouseOver={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
       >
-        {getTextClass(isHover)}
+        W
+        <span className={`${style.dynamic_text} ${getTextClass(isHover)}`}>
+          eather
+        </span>
+        W
+        <span className={`${style.dynamic_text} ${getTextClass(isHover)}`}>
+          hisper
+        </span>
       </h1>
-    </>
+    </Link>
   );
 };
 
